cfba7bfac6765a17a2093a3d980e3c9d9c972ed6
AndreRicardoé <contatoadmx@gmail.com>
2025-06-18 00:01:53 -0300

    mini minify

--- source/PhpMx/ViewRender/ViewRenderJs.php
+++ source/PhpMx/ViewRender/ViewRenderJs.php
@@ -23,6 +23,8 @@ abstract class ViewRenderJs extends ViewRender
 
         $content = self::applyPrepare($content);
 
+        $content = preg_replace(array("/\s+\n/", "/\n\s+/"), array("\n", "\n"), $content);
+
         if (count(self::__currentGet('imports')) > 1 || (count(self::$CURRENT) > 1 && !self::parentType('js')))
             $content = "<script>\n$content\n</script>";
 
