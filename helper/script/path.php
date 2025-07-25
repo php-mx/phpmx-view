016229ab38ae9450639cf4664a9c73c6925a6b1d
André Ricardo <contatoadmx@gmail.com>
2025-07-25 13:28:43 -0300

    cls

--- helper/script/path.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use PhpMx\Path;
-
-Path::register(dirname(__DIR__, 2));
--- helper/script/view.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use PhpMx\View;
-
-View::mediaStyle('tablet', 'screen and (min-width: 700px)');
-View::mediaStyle('desktop', 'screen and (min-width: 1200px)');
-View::mediaStyle('print', 'print');
-
-View::globalPrepare('VIEW', fn($ref, ...$params) => View::render($ref, [], ...$params));
