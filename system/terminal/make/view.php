08386bf828bbb95e5fe3edbc95aba8726d4f8028
André Ricardo <contatoadmx@gmail.com>
2025-07-26 15:28:36 -0300

    mx make.view

---
+++ system/terminal/make/view.php
@@ -0,0 +1,42 @@
+<?php
+
+use PhpMx\File;
+use PhpMx\Import;
+use PhpMx\Path;
+use PhpMx\Terminal;
+
+return new class extends Terminal {
+
+    function __invoke($view, $types = null)
+    {
+        $view = strToCamelCase($view);
+        $view = path("system/view", $view);
+
+        if ($types) {
+            $types = strtolower($types);
+            $types = explode(',', $types);
+            $types = array_map(fn($type) => trim($type), $types);
+            foreach ($types as $type)
+                $this->createViewFile("$view.$type", "library/template/terminal/view/$type.txt");
+        } else {
+            $this->createViewFile("$view.html", "library/template/terminal/view/full.txt");
+        }
+    }
+
+    protected function createViewFile($file, $template)
+    {
+        if (File::check($file))
+            return self::echo("[ignored] file [$file] already exists");
+
+        $content = Path::seekForFile($template);
+
+        if (!$content)
+            return self::echo("[ignored] template [$template] not found");
+
+        if ($content) {
+            $content = Import::content($content);
+            File::create($file, $content);
+            self::echo("[created] file [$file] created");
+        }
+    }
+};
