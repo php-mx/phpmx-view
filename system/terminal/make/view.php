7b24ab51ff07c9b23bbd23aacf4499ec0ec742ed
André Ricardo <contatoadmx@gmail.com>
2025-07-30 16:42:53 -0300

    mx make.view

--- system/terminal/make/view.php
+++ system/terminal/make/view.php
@@ -4,23 +4,45 @@ use PhpMx\File;
 use PhpMx\Import;
 use PhpMx\Path;
 use PhpMx\Terminal;
+use PhpMx\View;
 
 return new class extends Terminal {
 
-    function __invoke($view, $types = null)
+    function __invoke($view)
     {
+        $ex = str_contains($view, '.') ? File::getEx($view) : null;
+
+        if ($ex)
+            $view = substr($view, 0, -strlen($ex) - 1);
+
+        if ($ex == 'content') {
+            $ex = 'html';
+            $view = path($view, '_content');
+        }
+
+        if ($ex == 'style') {
+            $ex = 'css';
+            $view = path($view, '_style');
+        }
+
+        if ($ex == 'script') {
+            $ex = 'js';
+            $view = path($view, '_script');
+        }
+
+        if ($ex && !isset(View::$RENDER_CLASS[$ex]))
+            throw new Exception("View type [$ex] not supported");
+
         $view = explode('/', $view);
-        $view = array_map(fn($v) => str_replace(' ', '_', remove_accents($v)), $view);
+        $view = array_map(fn($v) => str_replace(' ', '_', remove_accents(trim($v))), $view);
         $view = path('system/view', ...$view);
 
-        if ($types) {
-            $types = explode(',', $types);
-            $types = array_map(fn($type) => trim(strtolower($type)), $types);
-            $types = array_values($types);
-            foreach ($types as $type)
-                $this->createViewFile("$view.$type", "library/template/terminal/view/$type.txt");
+        if ($ex) {
+            $this->createViewFile("$view.$ex", "library/template/terminal/view/$ex.txt");
         } else {
-            $this->createViewFile("$view.html", "library/template/terminal/view/full.txt");
+            $this->createViewFile(path($view, "_content.html"), "library/template/terminal/view/html.txt");
+            $this->createViewFile(path($view, "_style.css"), "library/template/terminal/view/css.txt");
+            $this->createViewFile(path($view, "_script.js"), "library/template/terminal/view/js.txt");
         }
     }
 
