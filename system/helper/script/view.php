ae343bbba211c1262272a8e6c3b5f697183091d9
André Ricardo <contatoadmx@gmail.com>
2026-04-21 13:09:42 -0300

    fx init view in new project

--- system/helper/script/view.php
+++ system/helper/script/view.php
@@ -2,8 +2,9 @@
 
 use PhpMx\View;
 
-View::mediaStyle('tablet', 'screen and (min-width: 700px)');
-View::mediaStyle('desktop', 'screen and (min-width: 1200px)');
-View::mediaStyle('print', 'print');
-
-View::globalPrepare('VIEW', fn($ref, ...$params) => View::render($ref, [], ...$params));
+if (class_exists(View::class)) {
+    View::mediaStyle('tablet', 'screen and (min-width: 700px)');
+    View::mediaStyle('desktop', 'screen and (min-width: 1200px)');
+    View::mediaStyle('print', 'print');
+    View::globalPrepare('VIEW', fn($ref, ...$params) => View::render($ref, [], ...$params));
+}
